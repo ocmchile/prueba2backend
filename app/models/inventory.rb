683cd47f6c94e7ec4d2dc5b32a5797df7093c854
Oscar Contreras <ocmchile@gmail.com>
2016-07-18 21:35:28 -0400

    Cascadas

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,6 +1,6 @@
 class Inventory < ActiveRecord::Base
   belongs_to :category
-  belongs_to :work
+  belongs_to :work, dependent: :destroy
   validates :numserie, presence: true
   validates :tamrueda, numericality: {greater_than: 0}
   validates_uniqueness_of :numserie
